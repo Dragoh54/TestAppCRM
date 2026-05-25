509722985b2c688982542361c356becabeb782c3
Dragoh54 <ndragoh@gmail.com>
2026-05-25 19:04:50 +0300

    feat: add references, add nugets, add dbContext

---
+++ TestAppCRM.DataAccess/Context/TestAppCRMDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestAppCRM.DomainModel;
+
+namespace TestAppCRM.DataAccess.Context;
+
+public class TestAppCrmDbContext : DbContext
+{
+    public TestAppCrmDbContext(DbContextOptions<TestAppCrmDbContext> options)
+        : base(options)
+    {
+    }
+    
+    public DbSet<Contact> Contacts { get; set; }
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(
+            typeof(TestAppCrmDbContext).Assembly);
+        
+        base.OnModelCreating(modelBuilder);
+    }
+}
